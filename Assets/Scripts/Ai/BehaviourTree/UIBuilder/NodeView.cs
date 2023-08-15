274eaf3d860ef115dc75cc1ef28f7788a8c3d376
AIE\s200395 <s200395@students.aie.edu.au>
2023-08-15 16:54:10 +0930

    -Added new ruins models-Added palm trees-Did more testing in general-Jackson

---
+++ Assets/Scripts/Ai/BehaviourTree/UIBuilder/NodeView.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+using UnityEditor.Experimental.GraphView;
+using System;
+
+
+public class NodeView : UnityEditor.Experimental.GraphView.Node
+{
+    public Action<NodeView> OnNodeSelected;
+    public Node node;
+    public Port input;
+    public Port output;
+
+    public NodeView(Node node) : base("Assets/Scripts/Ai/BehaviourTree/UIBuilder/NodeView.uxml"){
+        this.node = node;
+        this.title = node.name;
+        this.viewDataKey = node.guid;
+        style.left = node.position.x;
+        style.top = node.position.y;
+
+        CreateInputPorts();
+        CreateOutputPorts();
+        SetUpClasses();
+
+        Label descriptionLabel = this.Q<Label>("description");
+        descriptionLabel.bindingPath = "description";
+        descriptionLabel.Bind(new UnityEditor.SerializedObject(node));
+    }
+
+    void SetUpClasses()
+    {
+        if (node is ActionNode)
+            AddToClassList("action");
+        else if (node is CompositeNode)
+            AddToClassList("composite");
+        else if (node is RootNode)
+            AddToClassList("root");
+        else if (node is DecoratorNode)
+            AddToClassList("decorator");
+        else if (node is SetterNode)
+            AddToClassList("setter");
+    }
+
+    void CreateInputPorts()
+    {
+        if (node is ActionNode)
+            input = InstantiatePort(Orientation.Vertical, Direction.Input, Port.Capacity.Single, typeof(bool));
+        else if (node is CompositeNode)
+            input = InstantiatePort(Orientation.Vertical, Direction.Input, Port.Capacity.Single, typeof(bool));
+        else if (node is RootNode) { }
+        //leaveBlank
+        else if (node is DecoratorNode)
+            input = InstantiatePort(Orientation.Vertical, Direction.Input, Port.Capacity.Single, typeof(bool));
+        else if (node is SetterNode)
+            input = InstantiatePort(Orientation.Vertical, Direction.Input, Port.Capacity.Single, typeof(bool));
+
+        if (input != null)
+        {
+            input.portName = "";
+            input.style.flexDirection = FlexDirection.Column;
+            inputContainer.Add(input);
+        }        
+    }
+
+    void CreateOutputPorts()
+    {
+        if (node is ActionNode)
+        {
+
+        }
+        else if (node is CompositeNode)
+        {
+            output = InstantiatePort(Orientation.Vertical, Direction.Output, Port.Capacity.Multi, typeof(bool));
+        }
+        else if (node is DecoratorNode)
+        {
+            output = InstantiatePort(Orientation.Vertical, Direction.Output, Port.Capacity.Single, typeof(bool));
+        }
+        else if (node is SetterNode)
+        {
+            output = InstantiatePort(Orientation.Vertical, Direction.Output, Port.Capacity.Single, typeof(bool));
+        }
+        else if (node is RootNode)
+        {
+            output = InstantiatePort(Orientation.Vertical, Direction.Output, Port.Capacity.Single, typeof(bool));
+        }
+
+        if (output != null)
+        {
+            output.portName = "";
+            output.style.flexDirection = FlexDirection.ColumnReverse;
+            outputContainer.Add(output);
+        }
+    }
+
+    public override void SetPosition(Rect newPos)
+    {
+        base.SetPosition(newPos);
+
+        node.position.x = newPos.xMin;
+        node.position.y = newPos.yMin;
+    }
+
+    public override void OnSelected()
+    {
+        base.OnSelected();
+        if(OnNodeSelected != null)
+            OnNodeSelected.Invoke(this);
+    }
+}
