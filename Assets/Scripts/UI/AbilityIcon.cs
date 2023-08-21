7611d08b37a1f6a6cc2d22ef6c6de1001ce0af56
Jake Price <37175719+Pricetag098@users.noreply.github.com>
2023-08-21 17:15:17 +0930

    Better ui

---
+++ Assets/Scripts/UI/AbilityIcon.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityIcon : MonoBehaviour
+{
+    [SerializeField]Ability ability;
+    Image icon;
+    [SerializeField]Image cooldown;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        icon = GetComponent<Image>();
+        cooldown = transform.GetChild(0).GetComponent<Image>();
+    }
+
+    public void SetAbility( Ability ability)
+	{
+        this.ability = ability;
+        icon.sprite = ability.icon;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        cooldown.fillAmount = 1- ability.GetCoolDownPercent();
+    }
+}
