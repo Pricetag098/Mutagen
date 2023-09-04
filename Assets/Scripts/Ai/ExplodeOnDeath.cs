728cf5c48e7f468fdeda8501cf1328df0c084831
WadeSmallAIE <s214772@students.aie.edu.au>
2023-09-04 21:33:13 +0930

    File Clean up whoever keeps importing deers stop it thee's like 40 of them

---
+++ Assets/Scripts/Ai/ExplodeOnDeath.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeOnDeath : MonoBehaviour
+{
+    [SerializeField] LayerMask targetLayers;
+    [SerializeField] float radius;
+    [SerializeField] DamageData damage;
+    
+    [SerializeField] VfxSpawnRequest vfx;
+    [SerializeField] DamageData damage2;
+    [SerializeField] VfxSpawnRequest vfx2;
+    [SerializeField] Element element;
+
+    private void Awake()
+    {
+        GetComponent<Health>().OnDeath += Explode;
+    }
+
+    void Explode(DamageData data)
+    {
+
+        if(data.type == element)
+        {
+            vfx2.Play(transform.position, Vector3.up);
+            List<Health> healths = new List<Health>();
+            Collider[] hits = Physics.OverlapSphere(transform.position, radius, targetLayers);
+            foreach (Collider hit in hits)
+            {
+                HitBox hb;
+                if (hit.GetComponent<Collider>().TryGetComponent(out hb))
+                {
+                    hb.OnHit(damage2);
+                }
+            }
+            GetComponent<PooledObject>().Despawn();
+        }
+        else
+        {
+            vfx.Play(transform.position, Vector3.up);
+            List<Health> healths = new List<Health>();
+            Collider[] hits = Physics.OverlapSphere(transform.position, radius, targetLayers);
+            foreach (Collider hit in hits)
+            {
+                HitBox hb;
+                if (hit.GetComponent<Collider>().TryGetComponent(out hb))
+                {
+                    hb.OnHit(damage);
+                }
+            }
+            GetComponent<PooledObject>().Despawn();
+        }
+        
+    }
+
+
+}
---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class HealthBar : MonoBehaviour
+{
+    public Health health;
+    public Image image;
+    public TMP_Text text;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.fillAmount = health.health/health.maxHealth;
+        text.text = health.health + " / " + health.maxHealth;
+    }
+}
