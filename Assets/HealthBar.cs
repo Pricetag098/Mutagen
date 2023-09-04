64003b09b80317c86e342c935d9c9c395d92de1b
Jake Price <37175719+Pricetag098@users.noreply.github.com>
2023-09-04 19:37:28 +0930

    Health bar

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthBar : MonoBehaviour
+{
+    public Health health;
+    public Image image;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.fillAmount = health.health/health.maxHealth;
+    }
+}
