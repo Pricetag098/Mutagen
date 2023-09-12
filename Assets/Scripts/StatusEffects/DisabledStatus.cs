96277644568a0e22b7c6ed41fefb1af26f576e68
Jake Price <37175719+Pricetag098@users.noreply.github.com>
2023-09-12 16:46:51 +0930

    New abilitys

---
+++ Assets/Scripts/StatusEffects/DisabledStatus.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "StatusEffects/Disabled")]
+public class DisabledStatus : StatusEffect
+{
+    public float duration;
+
+    Timer timer;
+    protected override void OnAdd()
+    {
+        timer = new Timer(duration, false);
+        health.GetComponent<AbilityCaster>().castBool = false;
+    }
+
+    public override void Tick()
+    {
+        if(timer.complete)
+            health.RemoveStatusEffect(this);
+        
+    }
+    public override void Combine(StatusEffect effect)
+    {
+        DisabledStatus status=effect as DisabledStatus;
+        timer.maxTime += status.duration;
+    }
+    protected override void OnRemove()
+    {
+        health.GetComponent<AbilityCaster>().castBool = true;
+    }
+}
