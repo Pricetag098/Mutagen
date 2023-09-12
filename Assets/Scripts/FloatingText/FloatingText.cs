7c20ad4471485649318205ab1cd7255f9b7d224f
JasonAnderson0 <s201293@students.aie.edu.au>
2023-09-12 15:48:48 +0930

    working on tank ai

--- Assets/Scripts/FloatingText/FloatingText.cs
+++ Assets/Scripts/FloatingText/FloatingText.cs
@@ -15,6 +15,8 @@ public class FloatingText
     public float motionSpeed;
     public float followStrength;
     public FloatingTextManager manager;
+    public float damage;
+    public bool canMerge = true;
     
 
     public void Show()
@@ -22,7 +24,7 @@ public class FloatingText
         active = true;
         lastShown = Time.time;
         go.SetActive(active);
-
+        canMerge = true;
         Vector3 curDev = new Vector3(Random.Range(-deviation, deviation), Random.Range(-deviation, deviation), Random.Range(-deviation, deviation));       
         go.transform.position = Camera.main.WorldToScreenPoint(follow.transform.position + curDev);
     }
@@ -44,12 +46,33 @@ public class FloatingText
             Hide();
         }
 
+        //foreach (FloatingText text in manager.floatingTexts)
+        //{
+        //    if (text.active && text.follow == follow)
+        //    {
+        //        if (text.canMerge)
+        //        {
+        //            if (Vector3.Distance(go.transform.position, text.go.transform.position) < 0.01f)
+        //            {
+        //                damage += text.damage;
+        //                if (damage <= 9)
+        //                    txt.text = damage.ToString().Substring(0, 1);
+        //                else if (damage < 100)
+        //                    txt.text = damage.ToString().Substring(0, 2);
+        //                else
+        //                    txt.text = damage.ToString().Substring(0, 3);
+
+        //                canMerge = false;
+        //                text.Hide();
+        //            }
+        //        }
+        //    }
+        //}
+
         if (follow)
         {
             go.transform.position += ((Camera.main.WorldToScreenPoint(follow.transform.position)) - go.transform.position).normalized * followStrength;
         }
-
         go.transform.position += motion * motionSpeed * Time.deltaTime;
-
     }
 }
