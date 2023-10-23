72679c5f8edbaec91f0766544b199dd89e64a84a
Jake Price <37175719+Pricetag098@users.noreply.github.com>
2023-10-23 14:09:46 +1030

    ui

--- Assets/Scripts/UI/SelectionUi.cs
+++ Assets/Scripts/UI/SelectionUi.cs
@@ -15,13 +15,16 @@ public class SelectionUi : MonoBehaviour
     public float openTime = 0.1f;
     public float buttonEntryTime = .1f;
     public float buttonEntranceDelay = .05f;
+    PlayerAbilityCaster playerAbilityCaster;
     //public float buttonShowDelay = 1;
     public Vector3 buttonOffset;
     public Volume ppVolume;
-    private void Start()
+    Ability[] abilityOptions;
+    private void Awake()
     {
+        playerAbilityCaster = FindObjectOfType<PlayerAbilityCaster>();
         buttons = GetComponentsInChildren<SelectionButton>();
-
+        DOTween.defaultTimeScaleIndependent = true;
         Close();
 		DOTween.Kill(this, true);
 	}
@@ -29,29 +32,43 @@ public class SelectionUi : MonoBehaviour
 
     public void OpenWith(Ability[] abilities)
     {
-
+        for(int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].SetValues(abilities[i], playerAbilityCaster.caster.abilities[(int)abilities[i].slotMask]);
+        }
+        abilityOptions = abilities;
+        Open();
     }
 
-
+    public void Select(int index)
+    {
+        playerAbilityCaster.SetAbility(abilityOptions[index],(int)abilityOptions[index].slotMask);
+        //gameObject.SetActive(false);
+        Close();
+    }
     [ContextMenu("open")]
     public void Open()
     {
+        
         DOTween.Kill(this, true);
         openSequence = DOTween.Sequence(this);
+        
 		for (int i = 0; i < buttons.Length; i++)
 		{
 			openSequence.Append(buttons[i].GetComponent<RectTransform>().DOAnchorPos(buttons[i].startPosition + buttonOffset, 0));
 		}
 		openSequence.Append(transform.DOScaleX(1, openTime));
         openSequence.Join(DOTween.To(() => ppVolume.weight, x => ppVolume.weight = x, 1, openTime));
-		
-		//openSequence.AppendInterval(openTime);
-		for (int i = 0; i < buttons.Length; i++)
+        openSequence.Join(DOTween.To(() => Time.timeScale, x => Time.timeScale = x, 0, openTime));
+
+        //openSequence.AppendInterval(openTime);
+        for (int i = 0; i < buttons.Length; i++)
         {
 			
 			openSequence.Append(buttons[i].GetComponent<RectTransform>().DOAnchorPos(buttons[i].startPosition, buttonEntryTime)).SetEase(Ease.InSine);
             openSequence.AppendInterval(buttonEntranceDelay);
         }
+        
         
     }
     [ContextMenu("close")]
@@ -73,7 +90,7 @@ public class SelectionUi : MonoBehaviour
 
         closeSequence.Append(transform.DOScaleX(0, openTime));
         closeSequence.Join(DOTween.To(() => ppVolume.weight, x => ppVolume.weight = x, 0, openTime));
-
+        closeSequence.Join(DOTween.To(() => Time.timeScale, x => Time.timeScale = x, 1, openTime));
 
     }
 
