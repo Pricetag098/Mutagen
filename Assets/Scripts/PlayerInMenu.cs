5b1d8785abfd7a9d5059ab83a3cdb889c43cb868
Jake Price <37175719+Pricetag098@users.noreply.github.com>
2023-10-30 16:50:55 +1030

    Ui

--- Assets/Scripts/PlayerInMenu.cs
+++ Assets/Scripts/PlayerInMenu.cs
@@ -7,9 +7,11 @@ public class PlayerInMenu : MonoBehaviour
 {
 
     public Rigidbody player;
+    public CanvasGroup canvas,playerUi;
+    public float fadeTime;
     public float animationTime;
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
         DisablePlayer();
     }
@@ -18,19 +20,23 @@ public class PlayerInMenu : MonoBehaviour
     {
         player.isKinematic = true;
         player.GetComponent<PlayerAim>().enabled = false;
+        player.GetComponent<PlayerAbilityCaster>().enabled = false;
         player.GetComponentInChildren<Animator>().SetTrigger("Sit");
     }
 
-    void EnablePLayer()
+    public void EnablePLayer()
     {
         
         player.GetComponentInChildren<Animator>().SetTrigger("Stand");
         Sequence sequence = DOTween.Sequence();
+        sequence.Append(canvas.DOFade(0,fadeTime));
+        sequence.Join(playerUi.DOFade(1, fadeTime));
         sequence.AppendInterval(animationTime);
         sequence.AppendCallback(() =>
         {
             player.isKinematic = false;
             player.GetComponent<PlayerAim>().enabled = true;
+            player.GetComponent<PlayerAbilityCaster>().enabled = true;
         });
     }
 
