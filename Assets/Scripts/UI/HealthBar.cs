88f66f125b2dcabedbc596d5d8ea4c2ef382afdd
AIE\s214772 <s214772@students.aie.edu.au>
2023-11-01 12:08:08 +1030

    Hit effect and health bar ui updated

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -3,17 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using JetBrains.Annotations;
+using DG.Tweening;
+
 public class HealthBar : MonoBehaviour
 {
     public Health health;
-    public Image image;
+    public Image hBar;
+    public Image dBar;
     public TMP_Text text;
-    
+
+    private void Start()
+    {
+        health.OnHit += OnHit;
+
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        image.fillAmount = health.health/health.maxHealth;
+        hBar.fillAmount = health.health/health.maxHealth;
         text.text = (int)health.health + " / " + health.maxHealth;
+    }
+
+    public void OnHit(DamageData damage)
+    {
+        DoTween();
+    }
+
+
+    //Async example
+    public Image hitEffect;
+
+    public async void DoTween()
+    {
+        await hitEffect.DOFade(1, 1).AsyncWaitForCompletion();
+        await dBar.DOFillAmount(hBar.fillAmount, 3).AsyncWaitForCompletion();
+        await hitEffect.DOFade(0, 1).AsyncWaitForCompletion();
     }
 }
