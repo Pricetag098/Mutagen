1a8f61532fdf6e25c1b9ccd977fc8014af24fc55
JasonAnderson0 <s201293@students.aie.edu.au>
2023-11-02 23:02:39 +1030

    pickup system changed to drop object

--- Assets/Scripts/Interaction/AbilityPickupInteractable.cs
+++ Assets/Scripts/Interaction/AbilityPickupInteractable.cs
@@ -8,12 +8,12 @@ public class AbilityPickupInteractable : Interactable
 	[SerializeField] GameObject hoverText;
 	[SerializeField] Image icon;
 
-	//Jason Code
-	public Enemy agent;
+	////Jason Code
+	//public Enemy agent;
 	
 	public override void OnHover()
 	{
-		//hoverText.SetActive(true);
+		hoverText.SetActive(true);
 	}
 	public override void ExitHover()
 	{
@@ -38,7 +38,7 @@ public class AbilityPickupInteractable : Interactable
 		player.abilitySelector.OpenWith(abilitys);
 		//player.EquipAbility(ability);
 		interactor.hasTarget = false;
-		Destroy(agent.transform.parent.gameObject);
+		//Destroy(agent.transform.parent.gameObject);
 		enabled = false; 
 	}
 	private void Awake()
