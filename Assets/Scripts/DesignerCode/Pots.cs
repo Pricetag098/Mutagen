46dc845d94c42daff60a758a2186a6cb34b1827b
JasonAnderson0 <s201293@students.aie.edu.au>
2023-11-28 11:28:40 +1030

    adjusted drop pool, changed colliders on canyon

--- Assets/Scripts/DesignerCode/Pots.cs
+++ Assets/Scripts/DesignerCode/Pots.cs
@@ -6,25 +6,9 @@ public class Pots : MonoBehaviour
 {
     public VfxSpawnRequest vfx;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         vfx.Play(transform.position, transform.forward);
         Destroy(gameObject);
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        
     }
 }
