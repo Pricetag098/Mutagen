2b77438cd75adc8289ba9a7e738ffd761323284c
JasonAnderson0 <s201293@students.aie.edu.au>
2023-12-05 09:57:55 +1030

    working on UI

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -69,11 +69,8 @@ public class PauseMenu : MonoBehaviour
     }
     public void Open()
     {
-        Debug.Log("Open");
-
         //if (open)
         //    return;
-
 
         open=true;
         DOTween.Kill(this, true);
@@ -97,8 +94,6 @@ public class PauseMenu : MonoBehaviour
 
     public void Close()
     {
-        Debug.Log("Close");
-
         //if (!open)
         //    return;
         open = false;
